76a29d372a9185c3bac46ae35fd6958a4172ac54
Jack Liu Shurui <jackliusr@gmail.com>
2024-06-16 16:41:14 +0800

    final working version of Fraud Detection with the DataStream API

--- frauddetection/src/main/java/spendreport/FraudDetector.java
+++ frauddetection/src/main/java/spendreport/FraudDetector.java
@@ -18,11 +18,17 @@
 
 package spendreport;
 
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.util.Collector;
 import org.apache.flink.walkthrough.common.entity.Alert;
 import org.apache.flink.walkthrough.common.entity.Transaction;
-
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.api.common.functions.OpenContext;
 /**
  * Skeleton code for implementing a fraud detector.
  */
@@ -34,15 +40,66 @@ public class FraudDetector extends KeyedProcessFunction<Long, Transaction, Alert
 	private static final double LARGE_AMOUNT = 500.00;
 	private static final long ONE_MINUTE = 60 * 1000;
 
+
+	private transient ValueState<Boolean> flagState;
+	private transient ValueState<Long> timerState;
+
+	@Override
+	public void open(OpenContext openContext){
+
+		ValueStateDescriptor<Boolean> flagDescriptor 
+		     = new ValueStateDescriptor<>("flag",Types.BOOLEAN);
+        flagState = getRuntimeContext().getState(flagDescriptor);
+
+        ValueStateDescriptor<Long> timerDescriptor = new ValueStateDescriptor<Long>(
+			"timer-state",
+			Types.LONG
+		);
+		timerState = getRuntimeContext().getState(timerDescriptor);
+	}
+
 	@Override
 	public void processElement(
 			Transaction transaction,
 			Context context,
 			Collector<Alert> collector) throws Exception {
 
-		Alert alert = new Alert();
-		alert.setId(transaction.getAccountId());
+        Boolean lastTransactionWasSmall = flagState.value();
+        
+		if(lastTransactionWasSmall != null){
+			if(transaction.getAmount() > LARGE_AMOUNT){
+				Alert alert = new Alert();
+				alert.setId(transaction.getAccountId());
+				collector.collect(alert);
+			}
 
-		collector.collect(alert);
+			cleanUp(context);
+		}
+	
+		if (transaction.getAmount()  < SMALL_AMOUNT) {
+			// Set the flag to true
+			flagState.update(true);
+
+			long timer = context.timerService().currentProcessingTime();
+			context.timerService().registerProcessingTimeTimer(timer);
+			timerState.update(timer);
+		}	
+	}
+
+	@Override
+	public void onTimer(long timestamp, OnTimerContext ctx, Collector<Alert> out) {
+		// remove flag after 1 minute
+		timerState.clear();
+		flagState.clear();
+	}
+
+	private void cleanUp(Context ctx) throws Exception {
+		// delete timer
+		Long timer = timerState.value();
+		ctx.timerService().deleteProcessingTimeTimer(timer);
+	
+		// clean up all state
+		timerState.clear();
+		flagState.clear();
 	}
 }
